2428c57c160f1e411ee0b916f1a2abfa6bf9d663
yogenyslav <evgenygurov9@mail.ru>
2023-06-25 13:47:29 +0300

    stop code

--- internal/service/ml.go
+++ internal/service/ml.go
@@ -10,6 +10,8 @@ import (
 	"profbuh/internal/config"
 	"profbuh/internal/logging"
 	"profbuh/internal/models"
+	"regexp"
+	"strconv"
 
 	"gorm.io/gorm"
 )
@@ -44,6 +46,44 @@ func GenerateArticle(ctx context.Context, db *gorm.DB, record models.Record) (mo
 		return models.MlResponse{}, err
 	}
 	logging.Log.Debugf("Unmarshalled: %v", mlResponse)
+
+	article := mlResponse.Body
+	timecodes := regexp.MustCompile(`\[(.*?)\]`).FindAllString(article, -1)
+
+	for filename, url := range mlResponse.Images {
+		logging.Log.Debugf("Filename: %v, URL: %v", filename, url)
+		for _, timecodePair := range timecodes {
+			curTimecodes := regexp.MustCompile(`-`).Split(timecodePair, -1)
+			start := regexp.MustCompile(":").Split(curTimecodes[0], -1)
+			startMinutes, _ := strconv.ParseFloat(start[0], 64)
+			startSecondsAndMili, _ := strconv.ParseFloat(start[1], 64)
+			startSecondsStr := regexp.MustCompile(".").Split(strconv.FormatFloat(startSecondsAndMili, 'f', -1, 64), -1)[0]
+			startSeconds, _ := strconv.ParseFloat(startSecondsStr, 64)
+			startDuration := 60*startMinutes + startSeconds
+
+			end := regexp.MustCompile(":").Split(curTimecodes[1], -1)
+			endMinutes, _ := strconv.ParseFloat(end[0], 64)
+			endSecondsAndMili, _ := strconv.ParseFloat(end[1], 64)
+			endSecondsStr := regexp.MustCompile(".").Split(strconv.FormatFloat(endSecondsAndMili, 'f', -1, 64), -1)[0]
+			endSeconds, _ := strconv.ParseFloat(endSecondsStr, 64)
+			endDuration := 60*endMinutes + endSeconds
+
+			filename := regexp.MustCompile(":").Split(filename, -1)
+			filenameHours, _ := strconv.ParseFloat(filename[0], 64)
+			filenameMinutes, _ := strconv.ParseFloat(filename[1], 64)
+			filenameSeconds, _ := strconv.ParseFloat(filename[2], 64)
+			filenameDuration := 60*60*filenameHours + 60*filenameMinutes + filenameSeconds
+
+			logging.Log.Debugf("Start: %v, End: %v, Filename: %v", startDuration, endDuration, filenameDuration)
+
+			if filenameDuration >= startDuration && filenameDuration <= endDuration {
+				article = regexp.MustCompile(`\[(.*?)\]`).ReplaceAllString(article, "<img src="+url+">")
+			}
+		}
+	}
+
+	logging.Log.Debugf("Article: %v", article)
+	mlResponse.Body = article
 
 	return mlResponse, nil
 }
